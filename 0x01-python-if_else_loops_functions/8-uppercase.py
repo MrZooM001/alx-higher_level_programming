804ac7120117a478c700b37a3e36bb31ee0295ee
Hazem Magdy <hhazem0010@gmail.com>
2023-09-06 10:13:53 -0700

    fix trailer character at the end

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,8 @@
 def uppercase(str):
     for c in str:
         if ord(c) > 96 and ord(c) < 123:
-            print("{}".format(chr(ord(c) - 32)), end="")
+            asci = 32
         elif ord(c) < 91 or ord(c) > 122:
-            print("{}".format(c), end="")
-    return ""
+            asci = 0
+        print("{:c}".format(ord(c) - asci), end="")
+    print()
