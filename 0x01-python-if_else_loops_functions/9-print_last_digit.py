cd8a3984dee30f671fabb0d1c2b479be9adc2138
Hazem Magdy <hhazem0010@gmail.com>
2023-09-06 10:27:14 -0700

    Fix single digit in case of return

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return abs(number) % 10
+    lst_dgt = abs(number) % 10
+    if number < 0:
+        lst_dgt = -lst_dgt
+    print("{:d}".format(lst_dgt), end="")
+    return lst_dgt
+
