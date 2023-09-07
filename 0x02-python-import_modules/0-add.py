54b9ff85523daa0ff149089a9febdf6663e0699d
Hazem Magdy <hhazem0010@gmail.com>
2023-09-07 12:58:27 -0700

    add 0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from add_0 import add
+
+a = 1
+b = 2
+
+if __name__ == "__main__":
+    sum = add(1, 2)
+    print("{} + {} = {}".format(a, b, sum))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+Projects of **0x02. Python - import & modules**
