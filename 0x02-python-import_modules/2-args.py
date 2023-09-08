cd80e12f1ec7783ff175684a9583e79d328f021c
Hazem Magdy <hhazem0010@gmail.com>
2023-09-08 06:15:27 -0700

    fix some logic and styling

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("{} argument.".format(len(sys.argv) - 1))
+    if len(sys.argv[1:]) == 0:
+        print("{} arguments.".format(len(sys.argv[1:])))
     else:
-        if len(sys.argv) == 2:
-            print("{} argument:".format(len(sys.argv) - 1))
+        if len(sys.argv[1:]) == 1:
+            print("{} argument:".format(len(sys.argv[1:])))
         else:
-            print("{} arguments:".format(len(sys.argv) - 1))
-        
-        for i in range(len(sys.argv) - 1):
+            print("{} arguments:".format(len(sys.argv[1:])))
+
+        for i in range(len(sys.argv[1:])):
             i += 1
             print("{}: {}".format(i, sys.argv[i]))
