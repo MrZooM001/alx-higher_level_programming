d2a0d433681d10ed3f955f26868eca6b732bccca
Hazem Magdy <hhazem0010@gmail.com>
2023-09-09 11:47:48 -0700

    add check for empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tup = (len(sentence), sentence[0])
+    tup = ()
+    if len(sentence) == 0:
+        tup = (len(sentence), None)
+    else:
+        tup = (len(sentence), sentence[0])
     return tup
