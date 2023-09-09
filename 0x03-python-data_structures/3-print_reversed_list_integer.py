d156e6b50ac452f4460a612d219c3fe8ec59a1c7
Hazem Magdy <hhazem0010@gmail.com>
2023-09-09 04:15:09 -0700

    add check if list is none

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     new_list = my_list[::-1]
     for i in range(len(new_list)):
         print("{:d}".format(new_list[i]))
