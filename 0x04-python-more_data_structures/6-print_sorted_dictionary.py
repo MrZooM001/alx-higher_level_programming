0079d635d9be81ac8c373fd94ce3539a94ab216e
Hazem Magdy <hhazem0010@gmail.com>
2023-09-14 12:42:53 -0700

    fix funcs 6 & 7

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys =  list(a_dictionary.keys())
+    sorted_keys = list(a_dictionary.keys())
     sorted_keys.sort()
     sorted_dict = {i: a_dictionary.get(i) for i in sorted_keys}
     print(sorted_dict)
