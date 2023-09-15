9a46181e1882cefd336d07868ac5654a7e9f4844
Hazem Magdy <hhazem0010@gmail.com>
2023-09-15 12:32:21 -0700

    fix palindrom style

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,29 +4,6 @@
 #include "lists.h"
 
 /**
- * reverse_list - reverse a singly linked list
- * @head: pointer to head of list
- *
- * Return: reversed list
- */
-listint_t *reverse_list(listint_t *head)
-{
-listint_t *previous_node, *current_node, *next_node;
-
-previous_node = NULL;
-current_node = head;
-next_node = NULL;
-while (current_node != NULL)
-{
-next_node = current_node->next;
-current_node->next = previous_node;
-previous_node = current_node;
-current_node = next_node;
-}
-return (previous_node);
-}
-
-/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to head of list
  *
@@ -36,42 +13,52 @@ return (previous_node);
 int is_palindrome(listint_t **head)
 {
 listint_t *start, *end, *middle, *previous_start, *first_half, *second_half;
+int is_palindrome;
 
 start = *head, end = *head;
 previous_start = NULL, middle = NULL;
-first_half = *head;
+first_half = *head, is_palindrome = 1;
 if (*head == NULL || (*head)->next == NULL)
 return (1);
 
 while (end != NULL && end->next != NULL)
 {
 end = end->next->next;
+listint_t *temp = start->next;
+start->next = previous_start;
 previous_start = start;
-start = start->next;
+start = temp;
 }
 if (end != NULL)
 {
 middle = start;
 start = start->next;
 }
-second_half = reverse_list(start);
+second_half = start;
+first_half = previous_start;
 while (second_half != NULL)
 {
 if (first_half->n != second_half->n)
-return (0);
-
+{
+is_palindrome = 0;
+break;
+}
 first_half = first_half->next;
 second_half = second_half->next;
 }
-
-reverse_list(second_half);
+previous_start = NULL;
+while (first_half != NULL)
+{
+listint_t *temp = first_half->next;
+first_half->next = previous_start;
+previous_start = first_half;
+first_half = temp;
+}
 if (middle != NULL)
 {
-previous_start->next = middle;
+previous_start = middle;
 middle->next = second_half;
 }
-else
-previous_start->next = second_half;
 
-return (1);
+return (is_palindrome);
 }
