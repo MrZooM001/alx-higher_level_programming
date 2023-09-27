2c86b5e4398e8df3bffb0a23bc284252d229f5e0
Hazem Magdy <hhazem0010@gmail.com>
2023-09-27 16:21:33 -0700

    fix docstring task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 class Square:
     """
+    python3 -c 'print(__import__("class").Square.__doc__)'
+
     A class that defines a square
     """
-    pass
+    def __init__(self) -> None:
+        pass
