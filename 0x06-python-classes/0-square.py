cf4c1d48d1f2befe1134759ebba8af8149eada8d
Hazem Magdy <hhazem0010@gmail.com>
2023-09-27 16:24:32 -0700

    fix 2

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """
-    python3 -c 'print(__import__("class").Square.__doc__)'
-
-    A class that defines a square
-    """
-    def __init__(self) -> None:
-        pass
+    """An empty class that defines a square"""
+    pass
