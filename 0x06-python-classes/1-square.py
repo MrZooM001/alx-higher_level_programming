b0a67b4b05f010f569117b9628544dcc03ce3673
Hazem Magdy <hhazem0010@gmail.com>
2023-09-27 17:04:57 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """A class to defines a square based on 0-square.py"""
+
+
+    def __init__(self, size):
+        """Initialization of the square"""
+        self.__size = size
