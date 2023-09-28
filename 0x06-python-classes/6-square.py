09f780b1b4e13db378748a0a795118b1268c084c
Hazem Magdy <hhazem0010@gmail.com>
2023-09-27 19:32:28 -0700

    fix 6x6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,8 +49,8 @@ class Square:
             for space in range(self.__position[1]):
                 print()
             for i in range(self.__size):
-                print("#" * self.__size)
                 print(" " * self.__position[0], end="")
+                print("#" * self.__size)
 
     @property
     def position(self):
@@ -60,9 +60,9 @@ class Square:
     @position.setter
     def position(self, value):
         """Set a new position's values of the square"""
-        if (type(value) is not tuple or len(value) != 2
-                or type(value[0]) is not int or value[0] < 0
-                or type(value[1]) is not int or value[1] < 0):
+        if (not isinstance(value, tuple) or len(value) != 2
+                or type(value[0]) is not int or type(value[1]) is not int
+                or value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
