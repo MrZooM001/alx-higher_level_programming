32527ee8b53cd75a37124c57ece64a554d58f601
Hazem Magdy <hhazem0010@gmail.com>
2023-09-27 17:32:51 -0700

    fix task 1.1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,8 +4,6 @@
 
 class Square:
     """A class to defines a square based on 0-square.py"""
-
-
     def __init__(self, size):
         """Initialization of the square"""
         self.__size = size
