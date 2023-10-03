a99fb6f0e7597b3208d31ef8accd6cdd1541c840
Hazem Magdy <hhazem0010@gmail.com>
2023-10-03 14:22:07 -0700

    fix task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Module with no class or object attribute"""
+
+
 class LockedClass:
+    """Prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name"""
+
     def __setattr__(self, name, value):
         if name == "first_name":
             self.__dict__[name] = value
