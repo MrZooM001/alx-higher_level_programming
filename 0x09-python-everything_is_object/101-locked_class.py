b50118ac8084934df9e842b6a5b469310025dfcd
Hazem Magdy <hhazem0010@gmail.com>
2023-10-04 00:55:17 -0700

    fix 19 & 25

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,8 +4,8 @@
 
 class LockedClass:
     """Prevents creating new instance attributes dynamically"""
-
     def __setattr__(self, name, value):
+        """Only set when attribute's name == firt_name"""
         if name == "first_name":
             self.__dict__[name] = value
         else:
