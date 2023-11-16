124dc295ce3af0b64029890580ea4d8346741df8
Hazem Magdy <hhazem0010@gmail.com>
2023-11-16 14:36:43 -0800

    fix 9, 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,4 +5,4 @@ LEFT JOIN `tv_show_genres` AS `show_genre`
 ON tv_shows.id = show_genre.show_id
 LEFT JOIN `tv_genres` AS `genre`
 ON show_genre.genre_id = genre.id
-ORDER BY `title` ASC. `name` ASC;
+ORDER BY `title` ASC, `name` ASC;
