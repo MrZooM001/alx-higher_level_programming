18acf5ebd294d67bb3d357048208a04b019664ef
Hazem Magdy <hhazem0010@gmail.com>
2023-12-16 08:08:32 -0800

    init & tasks 1, 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module to list all states from the database hbtn_0e_0_usa"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    connection = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=argv[1], passwd=argv[2],
+        database=argv[3], charset="utf8")
+
+    with connection.cursor() as cur:
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+        query_rows = cur.fetchall()
+
+        for row in query_rows:
+            print(row)
+
+    connection.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,6 @@
+# ***Project*** &nbsp; → &nbsp; **0x0F. Python - Object-relational mapping** <br />
+***Scope*** &nbsp; → &nbsp; ` Python ` ` OOP ` ` SQL ` ` MySQL ` ` ORM ` ` SQLAlchemy ` <br />
+
+* ## ***Tasks***
+* **[0. Get all states](./0-select_states.py)** &nbsp; → &nbsp; a script that lists all `states` from the database `hbtn_0e_0_usa` .
+* **[1. Filter states](./1-filter_states.py)** &nbsp; → &nbsp; a script that lists all `states` with a `name` starting with `N` (upper N) from the database `hbtn_0e_0_usa` .
