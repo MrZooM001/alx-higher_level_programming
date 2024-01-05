ea66a4c121e49c22a79013971acc17f1db813435
Hazem Magdy <hhazem0010@gmail.com>
2024-01-05 15:03:04 -0800

    fix task 5

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -Is -L "$1" -H 'X-School-User-Id: 98'
+curl -s --request GET -H 'X-School-User-Id: 98' "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -Is -L "$1" --request POST -H 'email: test@gmail.com' -H 'subject: I will always be here for PLD'
+curl -s --request POST -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
