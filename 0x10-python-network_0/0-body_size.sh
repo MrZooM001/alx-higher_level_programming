26b5f1d73b01b12f3e5586a7c8736743d0a915df
Hazem Magdy <hhazem0010@gmail.com>
2024-01-05 14:02:19 -0800

    fix 2 task 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# a script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-curl -s "$1" | wc -c
+# a script that takes in a URL, sends a request to that URL
+curl -Is "$1" | awk '/^Content-Length/ {print $2}'
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# a script that takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
-code=$(curl -Is "$1" | awk '/^HTTP/ {print $2}')
-if [ "$code" -eq '200' ]; then
-    curl "$1"
-fi
+# a script that takes in a URL, sends a GET request to the URL
+curl -s -L "$1"
