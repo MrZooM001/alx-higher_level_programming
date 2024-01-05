aff69a33f56080f964662ad806f524b06c954351
Hazem Magdy <hhazem0010@gmail.com>
2024-01-05 15:32:15 -0800

    fix task 7

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -s -L -w "%{http_code}\n" -o /dev/null "$1"
+curl -s -w "%{http_code}" -o /dev/null "$1"
