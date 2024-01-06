e15a795e51e99968329a59ac720becdca21ebb4c
Hazem Magdy <hhazem0010@gmail.com>
2024-01-05 16:23:09 -0800

    fix task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that makes a request that causes the server to respond with a message containing You got me!, in the body of the response
-curl -s -L 0.0.0.0:5000/catch_me --request POST -H "Origin: You got me!"
+curl -s -L --request POST -H "Origin: You got me!" 0.0.0.0:5000/catch_me
