a170fc5fac6ce1f019ea4fbfc8ae7cb71f913209
Hazem Magdy <hhazem0010@gmail.com>
2024-01-12 14:54:00 -0800

    fix task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,10 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     response = requests.get(url)
     res_json = response.json()
-    for i in range(11):
-        sha = res_json[i]['sha']
-        author = res_json[i]['commit']['author']['name']
-        print("{}: {}".format(sha, author))
+    i = 0
+    for key in res_json:
+        if i < 10:
+            sha = res_json[i]['sha']
+            author = res_json[i]['commit']['author']['name']
+            print("{}: {}".format(sha, author))
+        i += 1
