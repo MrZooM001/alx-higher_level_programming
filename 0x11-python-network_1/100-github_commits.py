8cd827547bcedd121400c799ac0b6e6f2ae8a5e1
Hazem Magdy <hhazem0010@gmail.com>
2024-01-12 14:42:57 -0800

    task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id."""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    response = requests.get(url)
+    res_json = response.json()
+    for i in range(10):
+        sha = res_json[i]['sha']
+        author = res_json[i]['commit']['author']['name']
+        print("{}: {}".format(sha, author))
