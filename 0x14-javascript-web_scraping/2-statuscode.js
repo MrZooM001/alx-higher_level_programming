911444e49004661ec6aa186078ca2ffa4b75b71e
Hazem Magdy <hhazem0010@gmail.com>
2024-01-31 07:22:05 +0200

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const requrest = require('request');
+const argv = process.argv;
+
+const url = argv[2];
+requrest(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(response.statusCode);
+});
