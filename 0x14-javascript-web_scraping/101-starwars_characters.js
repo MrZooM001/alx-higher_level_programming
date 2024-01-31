0325969b94360e80a3fec0e6aec095676ae799ae
Hazem Magdy <hhazem0010@gmail.com>
2024-01-31 10:27:07 +0200

    fix task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,18 +6,21 @@ const movieId = argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 requrest(url, (err, response, body) => {
-  if (err) {
-    console.log(err);
+  if (!err) {
+    const parse = JSON.parse(body);
+    const characters = parse.characters;
+    printNames(characters, 0);
   }
-  const parse = JSON.parse(body);
-  const characters = parse.characters;
-  characters.forEach(characterUrl => {
-    requrest(characterUrl, (err, response, body) => {
-      if (err) {
-        console.log(err);
-      }
+});
+
+function printNames (characterUrl, index) {
+  requrest(characterUrl[index], (err, response, body) => {
+    if (!err) {
       const character = JSON.parse(body);
       console.log(character.name);
-    });
+      if (index + 1 < characterUrl.length) {
+        printNames(characterUrl, index + 1);
+      }
+    }
   });
-});
+}
